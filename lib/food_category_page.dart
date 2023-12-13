57d22be89248cf634137d83284d7a267933786cd
hyukggun <hyukggunchoe@gmail.com>
2023-12-13 22:53:52 +0900

    feat: Add image property to Menu class and fetch restaurants
    1. Added an optional 'image' property to the Menu class in menu.dart.
    2. Created a mock JSON response for restaurants.
    3. Added a fetchRestaurants() function in food_category_page.dart to fetch restaurant data.
    4. Added factory constructors to the Restaurant and Menu classes to convert JSON to a list of Restaurant objects.
    5. Updated initState() in _FoodCategoryPageState to call fetchRestaurants() asynchronously.

--- lib/food_category_page.dart
+++ lib/food_category_page.dart
@@ -1,16 +1,62 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:pig_road/model/restaurant.dart';
+import 'package:pig_road/restaurant_card.dart';
 import 'food_category.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 
 class FoodCategoryPage extends StatefulWidget {
-  const FoodCategoryPage({super.key, required this.category});
+  const FoodCategoryPage({super.key, required this.category, this.restaurants = const <Restaurant>[] });
 
   final FoodCategory category;
+  final List<Restaurant> restaurants;
+
+  Future<List<Restaurant>> fetchRestaurants() async {
+    final response = await http.get(Uri.parse('https://run.mocky.io/v3/63b4fcf5-f873-4cfb-8a13-c1d7a9d5fee6'));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      List<Restaurant> restaurants = body['restaurants'].map<Restaurant>((restaurant) => Restaurant.fromJson(restaurant)).toList();
+      return restaurants;
+    } else {
+      print('Failed to load restaurants');
+      return [];
+    }
+  }
 
   @override
   State<FoodCategoryPage> createState() => _FoodCategoryPageState();
 }
 
+class RestaurantService {
+  Future<List<Restaurant>> fetchRestaurants() async {
+    final response = await http.get(Uri.parse('https://run.mocky.io/v3/63b4fcf5-f873-4cfb-8a13-c1d7a9d5fee6'));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      List<Restaurant> restaurants = body['restaurants'].map<Restaurant>((restaurant) => Restaurant.fromJson(restaurant)).toList();
+      return restaurants;
+    } else {
+      print('Failed to load restaurants');
+      return [];
+    }
+  }
+}
+
+
 class _FoodCategoryPageState extends State<FoodCategoryPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    widget.fetchRestaurants().then(
+      (value) => setState(() {
+        print(value);
+      })
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,16 +64,8 @@ class _FoodCategoryPageState extends State<FoodCategoryPage> {
         title: Text(widget.category.title),
       ),
       body: Column(
-        children: widget.category.store.foods.map((e) {
-          return SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: Card(
-                child: Center(
-                    child: Text('food name is $e'),
-                )
-            ),
-          );
+        children: widget.category.restaurantList.map((restaurant) {
+          return RestaurantCard(restaurant: restaurant);
         }).toList(),
       ),
     );
