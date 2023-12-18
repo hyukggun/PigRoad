3d60d4dee201c9a7767fb01c65bb9ef42a48af8c
hyukggun <hyukggunchoe@gmail.com>
2023-12-18 22:41:12 +0900

    Chore : GoogleMap 개발환경 구축

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSService.provideAPIKey("AIzaSyDGCtFxp_BC3Sfv8mkhW9Mig9Kbc3MyzOA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
