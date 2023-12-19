dc005215b55a912e282d969cd6ab561c5cdf0cdc
hyukggun <hyukggunchoe@gmail.com>
2023-12-19 22:17:39 +0900

    Refactor : simplfy main.dart
    1. main.dart 내 페이지 별도로 파일로 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pig_road/default_theme_card.dart';
-import 'package:pig_road/food_category.dart';
-import 'package:pig_road/food_category_page.dart';
-import 'package:pig_road/map_page.dart';
+import 'package:pig_road/page/main/main_page.dart';
 
+// @Main
 void main() {
   runApp(const MyApp());
 }
@@ -18,95 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: '돼지름길'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                  width: double.infinity,
-                  height: 100,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => const FoodCategoryPage(category: FoodCategory.western))
-                      );
-                    },
-                      child: DefaultThemeCard(
-                          child: Center(
-                              child: Text('양식'),
-                          ),
-                      )
-                  ),
-              ),
-              SizedBox(
-                  width: double.infinity,
-                  height: 100,
-                  child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => const FoodCategoryPage(category: FoodCategory.korean))
-                        );
-                      },
-                      child: DefaultThemeCard(
-                          child: Center(
-                              child: Text('한식'),
-                          ),
-                      ),
-                  ),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => const MapPage())
-                    );
-                  },
-                  child: const Text('go to the other page'))
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const MainPage(title: '돼지름길'),
     );
   }
 }
